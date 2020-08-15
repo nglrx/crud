451b671871ade2d4a59773f164d5317df090e4c8
Kapil Neurgaonkar <kapil.neurgaonkar@gmail.com>
2020-08-15 09:05:14 +0530

    Created new library 'crud'

---
+++ packages/crud/src/lib/crud.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nglrx-crud',
+  template: `
+    <p>
+      crud works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ packages/crud/src/lib/crud.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CrudComponent } from './crud.component';
+
+
+
+@NgModule({
+  declarations: [CrudComponent],
+  imports: [
+  ],
+  exports: [CrudComponent]
+})
+export class CrudModule { }
